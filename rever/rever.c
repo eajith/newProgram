ba4cf89c1d830c443f5b7b8112b456633239ffd0
eajith <eajithkumar128@gmail.com>
2016-06-06 12:16:42 +0530

    Update rever.c

--- rever/rever.c
+++ rever/rever.c
@@ -4,6 +4,14 @@ int main(void) {
 	char a[1001];
 	scanf("%s",a);
 	int i;
+	printf("output 1-")
+	for(i=strlen(a)-1;i>=0;i--){
+		
+			printf("%c",a[i]);
+	
+	}
+	printf("\noutput 2-");
+	
 	for(i=strlen(a)-1;i>=0;i--){
 		if(a[i]!='a' && a[i]!='e' && a[i]!='i' && a[i]!='o' && a[i]!='u'){
 			printf("%c",a[i]);
