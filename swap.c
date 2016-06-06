e459d381cb72862d1488a52466a5b2e43b151cf1
eajith <eajithkumar128@gmail.com>
2016-06-06 12:08:55 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-	char a[1000];
+	int len;
+	scanf("%d",&len);
+	char a[len+1];
 	scanf("%[^\n]s",a);
 	int i;
-	if(strlen(a)%2==0){
-	for(i=0;i<strlen(a);i+=2){
+	if(len%2==0){
+	for(i=0;i<len;i+=2){
 		char temp=a[i];
 		a[i]=a[i+1];
 		a[i+1]=temp;
 	}
 	}
 	else{
-		for(i=0;i<strlen(a)-1;i++){
+		for(i=0;i<len-1;i++){
 			char temp=a[i];
 			a[i]=a[i+1];
 			a[i+1]=temp		
