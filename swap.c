da2d3aa8f7bd6d79f0932230817d2f6ff102c9d6
eajith <eajithkumar128@gmail.com>
2016-06-06 12:02:12 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -4,10 +4,19 @@ int main(void) {
 	char a[1000];
 	scanf("%s",a);
 	int i;
+	if(strlen(a)%2==0){
 	for(i=0;i<strlen(a);i+=2){
 		char temp=a[i];
 		a[i]=a[i+1];
 		a[i+1]=temp;
+	}
+	}
+	else{
+		for(i=0;i<strlen(a)-1;i++){
+			char temp=a[i];
+			a[i]=a[i+1];
+			a[i+1]=temp		
+		}
 	}
 	printf("%s",a);
 	return 0;
