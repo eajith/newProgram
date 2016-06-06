cd61db7ac79b05ccf722df1763cc96701b05fbe1
eajith <eajithkumar128@gmail.com>
2016-06-06 12:10:10 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
-int main(void) {
-	int len;
-	scanf("%d",&len);
-	char a[len+1];
-	scanf("%[^\n]s",a);
-	int i;
-	if(len%2==0){
-	for(i=0;i<len;i+=2){
-		char temp=a[i];
-		a[i]=a[i+1];
-		a[i+1]=temp;
-	}
-	}
-	else{
-		for(i=0;i<len-1;i++){
-			char temp=a[i];
-			a[i]=a[i+1];
-			a[i+1]=temp		
-		}
-	}
-	printf("The reverse of string is:%s",a);
-	return 0;
+int main()
+{
+    int length,i;
+    scanf("%d",&length);
+    char str[length+1];
+    scanf("\n%[^\n]s",str);
+    if(length&1){
+        length=length-1;
+    }
+    for(i=0;i<length;i+=2){
+        char c;
+        c=str[i];
+        str[i]=str[i+1];
+        str[i+1]=c;
+    }
+    printf("%s",str);
+    return 0;
 }
